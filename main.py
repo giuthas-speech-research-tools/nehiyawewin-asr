7447babb69d7b3185a60d88b4f554953fab186f3
Pertti Palo <giuthas@gmail.com>
2026-04-26 20:22:32 -0600

    Old edits.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from datasets import load_dataset, DatasetDict
+
+
 def main():
     print("Hello from whisper-test!")
+    common_voice = DatasetDict()
+    common_voice["train"] = load_dataset(
+        path="cree-asr/hindi",
+        name="hi",
+        split="train+validation",
+        use_auth_token=True
+    )
+    common_voice["test"] = load_dataset(
+        path="cree-asr/hindi",
+        name="default",
+        split="test",
+        use_auth_token=True
+    )
+    print(common_voice)
 
 
 if __name__ == "__main__":
